871cfe6c7d2252c498f30d2d8ef625f774b46005
kav2345 <22hr1a3250@mtieat.org>
2024-09-19 01:39:14 -0700

    Update play.py

--- play.py
+++ play.py
@@ -30,3 +30,30 @@ Score: 110
 Overs played: 18
 '''
 
+
+def display_match_details():
+    # Input for Team 1
+    print("Team 1:")
+    team1_name = input("Team Name:\n")
+    team1_score = input("Score:\n")
+    team1_overs = input("Overs played:\n")
+    
+    # Input for Team 2
+    print("Team 2:")
+    team2_name = input("Team name:\n")
+    team2_score = input("Score:\n")
+    team2_overs = input("Overs played:\n")
+    
+    # Output match details
+    print("Match Details:")
+    print("Team 1:")
+    print(f"Name: {team1_name}")
+    print(f"Score: {team1_score}")
+    print(f"Overs played: {team1_overs}")
+    
+    print("Team 2:")
+    print(f"Name: {team2_name}")
+    print(f"Score: {team2_score}")
+    print(f"Overs played: {team2_overs}")
+
+display_match_details()
